d152d5ea35b9f2fdeaa51fdb4dc62ac559eeab7d
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-09-08 13:33:13 +0530

    Refactored the code using generic method

--- src/com/maximum/TestMaximum.java
+++ src/com/maximum/TestMaximum.java
@@ -1,120 +1,104 @@
 package com.maximum;
 
-public class TestMaximum {
-	private Integer firstNumber;
-	private Integer secondNumber;
-	private Integer thirdNumber;
+public class TestMaximum<T extends Comparable<T>> {
+	private T firstNumber;
+	private T secondNumber;
+	private T thirdNumber;
 
-	private Float floatNumber1;
-	private Float floatNumber2;
-	private Float floatNumber3;
+	private T floatNumber1;
+	private T floatNumber2;
+	private T floatNumber3;
 	
-	private String firstWord;
-	private String secondWord;
-	private String thirdWord;
+	private T firstWord;
+	private T secondWord;
+	private T thirdWord;
 	
 	
 
-	public TestMaximum(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
+	public TestMaximum(T firstNumber, T secondNumber, T thirdNumber) {
 		this.firstNumber = firstNumber;
 		this.secondNumber = secondNumber;
 		this.thirdNumber = thirdNumber;
 	}
 
-	public TestMaximum(Float floatNumber1, Float floatNumber2, Float floatNumber3) {
-		super();
-		this.floatNumber1 = floatNumber1;
-		this.floatNumber2 = floatNumber2;
-		this.floatNumber3 = floatNumber3;
-	}
-	
-	
-
-	public TestMaximum(String firstWord, String secondWord, String thirdWord) {
-		super();
-		this.firstWord = firstWord;
-		this.secondWord = secondWord;
-		this.thirdWord = thirdWord;
-	}
-
-	public Integer getFirstNumber() {
+	public T getFirstNumber() {
 		return firstNumber;
 	}
 
-	public void setFirstNumber(Integer firstNumber) {
+	public void setFirstNumber(T firstNumber) {
 		this.firstNumber = firstNumber;
 	}
 
-	public Integer getSecondNumber() {
+	public T getSecondNumber() {
 		return secondNumber;
 	}
 
-	public void setSecondNumber(Integer secondNumber) {
+	public void setSecondNumber(T secondNumber) {
 		this.secondNumber = secondNumber;
 	}
 
-	public Integer getThirdNumber() {
+	public T getThirdNumber() {
 		return thirdNumber;
 	}
 
-	public void setThirdNumber(Integer thirdNumber) {
+	public void setThirdNumber(T thirdNumber) {
 		this.thirdNumber = thirdNumber;
 	}
 
-	public Float getFloatNumber1() {
+	public T getFloatNumber1() {
 		return floatNumber1;
 	}
 
-	public void setFloatNumber1(Float floatNumber1) {
+	public void setFloatNumber1(T floatNumber1) {
 		this.floatNumber1 = floatNumber1;
 	}
 
-	public Float getFloatNumber2() {
+	public T getFloatNumber2() {
 		return floatNumber2;
 	}
 
-	public void setFloatNumber2(Float floatNumber2) {
+	public void setFloatNumber2(T floatNumber2) {
 		this.floatNumber2 = floatNumber2;
 	}
 
-	public Float getFloatNumber3() {
+	public T getFloatNumber3() {
 		return floatNumber3;
 	}
 
-	public void setFloatNumber3(Float floatNumber3) {
+	public void setFloatNumber3(T floatNumber3) {
 		this.floatNumber3 = floatNumber3;
 	}
 	
 	
 
-	public String getFirstWord() {
+	public T getFirstWord() {
 		return firstWord;
 	}
 
-	public void setFirstWord(String firstWord) {
+	public void setFirstWord(T firstWord) {
 		this.firstWord = firstWord;
 	}
 
-	public String getSecondWord() {
+	public T getSecondWord() {
 		return secondWord;
 	}
 
-	public void setSecondWord(String secondWord) {
+	public void setSecondWord(T secondWord) {
 		this.secondWord = secondWord;
 	}
 
-	public String getThirdWord() {
+	public T getThirdWord() {
 		return thirdWord;
 	}
 
-	public void setThirdWord(String thirdWord) {
+	public void setThirdWord(T thirdWord) {
 		this.thirdWord = thirdWord;
 	}
 
 	/*
 	 * method to find maximum of 3 integers
 	 */
-	public void findMaximum() {
+	public void findMaximum(T firstNumber, T secondNumber, T thirdNumber) {
 		if (firstNumber.compareTo(secondNumber) > 0 && firstNumber.compareTo(thirdNumber) > 0)
 			System.out.println("Maximum: " + firstNumber);
 		else if (secondNumber.compareTo(firstNumber) > 0 && secondNumber.compareTo(thirdNumber) > 0)
@@ -122,29 +106,4 @@ public class TestMaximum {
 		else
 			System.out.println("Maximum: " + thirdNumber);
 	}
-
-	/*
-	 * method to find maximum of 3 float
-	 */
-	public void findMaxOfFloat() {
-		if (floatNumber1.compareTo(floatNumber2) > 0 && floatNumber1.compareTo(floatNumber3) > 0)
-			System.out.println("Maximum: " + floatNumber1);
-		else if (floatNumber2.compareTo(floatNumber1) > 0 && floatNumber2.compareTo(floatNumber3) > 0)
-			System.out.println("Maximum: " + floatNumber2);
-		else
-			System.out.println("Maximum: " + floatNumber3);
-	}
-	
-	/*
-	 * method to find maximum of 3 float
-	 */
-	public void findMaxOfString() {
-		if (firstWord.compareTo(secondWord) > 0 && firstWord.compareTo(thirdWord) > 0)
-			System.out.println("Maximum: " + firstWord);
-		else if (secondWord.compareTo(firstWord) > 0 && secondWord.compareTo(thirdWord) > 0)
-			System.out.println("Maximum: " + secondWord);
-		else
-			System.out.println("Maximum: " + thirdWord);
-	}
-
 }
